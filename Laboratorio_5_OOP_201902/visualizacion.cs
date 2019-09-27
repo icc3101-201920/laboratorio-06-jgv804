8dd31fe0f4ee13f93459dc3077cae10f59e88a68
juan guillermo <jvergara5@miuandes.cl>
2019-09-27 09:34:35 -0300

    Show Hand terminado

---
+++ Laboratorio_5_OOP_201902/visualizacion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Laboratorio_5_OOP_201902
+{
+    public static class visualizacion
+    {
+
+        public static void ShowHand(Hand hand)
+        {
+            Console.setcursor(0, 14);
+            Console.Writeline("Hand: ");
+            string line = "";
+            for (int i = 0; i < hand.Cards.Count; i++)
+            {
+                if (hand.Cards[i].GetType().Name == nameof(CombatCard))
+                {
+                    Console.ForegroundColor = red;
+                    line = line + $"|({hand.Cards[i].Id}) " + hand.Cards[i].Name + $" {hand.Cards[i].type} |";
+                }
+                else
+                {
+                    Console.ForegroundColor = blue;
+                    line = line + $"|({hand.Cards[i].Id}) " + hand.Cards[i].Name + $" {hand.Cards[i].type} |";
+                }
+            }
+            Console.writeline(line);
+        }
+        public static void ShowDecks(List<Deck> decks)
+        {
+
+        }
+    }
+}
