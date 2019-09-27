5c86ffe2c568c97a6d59d64d7e0d06864e49d5f4
juan guillermo <jvergara5@miuandes.cl>
2019-09-27 09:48:53 -0300

    Show deck implementado

--- Laboratorio_5_OOP_201902/visualizacion.cs
+++ Laboratorio_5_OOP_201902/visualizacion.cs
@@ -10,26 +10,34 @@ namespace Laboratorio_5_OOP_201902
 
         public static void ShowHand(Hand hand)
         {
-            Console.setcursor(0, 14);
-            Console.Writeline("Hand: ");
+            Console.SetCursorPosition(0, 14);
+            Console.WriteLine("Hand: ");
             
             for (int i = 0; i < hand.Cards.Count; i++)
             {
                 if (hand.Cards[i].GetType().Name == nameof(CombatCard))
                 {
                     Console.ForegroundColor = red;
-                    Console.write( $"|({hand.Cards[i].Id}) " + hand.Cards[i].Name + $" {hand.Cards[i].type} :{hand.Cards[i].attackpoints} |");
+                    Console.Write( $"|({hand.Cards[i].Id}) " + hand.Cards[i].Name + $" {hand.Cards[i].type} :{hand.Cards[i].attackpoints} |");
                 }
                 else
                 {
                     Console.ForegroundColor = blue;
-                    Console.write( $"|({hand.Cards[i].Id}) " + hand.Cards[i].Name + $" {hand.Cards[i].type} |");
+                    Console.Write( $"|({hand.Cards[i].Id}) " + hand.Cards[i].Name + $" {hand.Cards[i].type} |");
                 }
+
             }
+            Console.Writeline();
             
         }
         public static void ShowDecks(List<Deck> decks)
         {
+            Console.SetCursorPosition(0, 2);
+            Console.WriteLine("select one deck:");
+            for (i = 0; i < decks.Count; i++)
+            {
+                Console.WriteLine($"{(i)} Deck {i + 1}");
+            }
 
         }
     }
